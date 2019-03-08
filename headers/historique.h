3edf135e5e4380a95df164dae1e698d321e92ae5
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-08 17:31:07 +0100

    Added missings semicolons

--- headers/historique.h
+++ headers/historique.h
@@ -4,8 +4,8 @@
 #include <plateau.h>
 
 int play_move(Board* board, Pcoord pcoord);
-void cancel_move(Board* board, Pcoord* ppcoord)
-void replay_move(Board* board, Pcoord* ppcoord)
+void cancel_move(Board* board, Pcoord* ppcoord);
+void replay_move(Board* board, Pcoord* ppcoord);
 
 #endif
 
