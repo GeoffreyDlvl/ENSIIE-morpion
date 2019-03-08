be0ad3f687178ce9c93180605bbd03095128ecdc
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2019-03-08 17:26:24 +0100

    Implement historique.h

--- headers/historique.h
+++ headers/historique.h
@@ -1,6 +1,10 @@
 #ifndef HISTORIQUE_H
 #define HISTORIQUE_H
 
+#include <plateau.h>
 
+int play_move(Board* board, Pcoord pcoord);
+void cancel_move(Board* board, Pcoord* ppcoord)
+void replay_move(Board* board, Pcoord* ppcoord)
 
 #endif
