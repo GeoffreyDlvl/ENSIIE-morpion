105bbaea77d2945d4863bf5c5b3c04da874ae706
Clement PERESSE <clement.peresse@pc251-17.pedago.ensiie.fr>
2019-03-08 17:40:48 +0100

    Added functions in interface.h

--- headers/interface.h
+++ headers/interface.h
@@ -1,8 +1,19 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#import"plateau.h"
-#import"historique.h"
+#include<plateau.h>
+
+void print_board(int hint,Board board*);
+
+int select_move(Board board*);
+
+void list_available_moves(Board board*);
+
+void ask_help(Board board*);
+
+void print_help();
+
+
 
 
 #endif
