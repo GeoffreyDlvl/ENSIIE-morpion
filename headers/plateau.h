07991ca2ca88cfa77f6082ff05f95012ea32b0d0
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-08 17:13:47 +0100

    Added all the functions and struct for plateau.h

--- headers/plateau.h
+++ headers/plateau.h
@@ -6,14 +6,29 @@ typedef struct
 {
     int x;
     int y;
-} coord;
+} Coord;
+
+typedef struct Coord* Pcoord; 
+
 
 typedef struct
 {
     int width;
     int heigh;
-    
-} board;
+    Pcoord** points;
+} Board;
+
+int check_file(char* path); 
+int read_file(Board* board, char* path); 
+void create_empty_board(Board* board); 
+Pcoord* get_valid_moves(Board* board);
+int is_move_valid(Board* board,Pcoord pcoord);
+Board* initialize_rand(int width,int height);
+int play_move(Board* board,Pcoord pcoord);
+int is_game_over(Board* board);
+
+
+
 
 
 #endif
