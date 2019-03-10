ac2bd8308c40f9560f6b89276fea9e4b6ea8ac45
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2019-03-10 17:52:14 +0100

    Updated plateau.h documentation

--- headers/plateau.h
+++ headers/plateau.h
@@ -3,100 +3,101 @@
 
 /**
  * \file plateau.h
- * \brief Manages the board and this type of data
+ * \brief This file handles the Board.
  * \authors Geoffrey DELVAL, Rémi GUIJARRO ESPINOSA, Gabriel MORAN, Clément PERESSE
  * \version lot_a
  * \date 03.2019 
+ *
+ * Handles the grid, points and lines on it.
+ *
  */
 
 /**
  * \struct Coord
- * \brief Object coordinates
- * Represent the coordinates couple (x,y) on the board
+ * \brief Coordinates on the Board
  */
-
 typedef struct 
 {
-    int x;
-    int y;
+    int x; /**< Horizontal address */
+    int y; /**< Vertical address */
 } Coord;
 
 /**
- * \brief Define the type Pcoord as a Coord pointer
+ * \brief Define the type Pcoord as a Coord pointer.
  */
-
 typedef struct Coord* Pcoord; 
-
-
 
 /**
  * \struct Board
- * \brief Object Board
- *  Represent the Board of the game with width, height and his 2d table of coord pointers
+ * \brief Represent the Board of the game with width, height and his 2d table of coord pointers.
  */
-
 typedef struct
 {
-    int width;
-    int height;
-    Pcoord** points;
+    int width; /**< Board width */
+    int height; /**< Board height */
+    Pcoord** points; /**< Bidimensionnal array of Pcoord */ 
 } Board;
 
 
 /**
  * \fn int check_file(char* path)
- * \brief Function which verifies the integrity and structure of a file
- * \param path : the path to the file to verify, can't be NULL
- * \return (1 : true)  if file structure is correct else return ( 0 : false )
+ * \brief Check the integrity and structure of a file.
+ *
+ * \param path : the path to the file to verify
+ * \return 1 if file structure is correct, 0 otherwise
  */
 int check_file(char* path); 
 
 /**
  * \fn int read_file(Board* pboard, char* path)
- * \brief Function which read a file and initialize the related Board
- * \param  pboard : A board pointer, can't be NULL
- * \param  path : the path to the file to verify, can't be NULL
- * \return (1 : true)  if no errors occured else return ( 0 : false )
+ * \brief Read the given file and initialize the Board accordingly.
+ *
+ * \param pboard Board pointer, SHOULD be NULL
+ * \param path File absolute path
+ * \return 1 if no error occured, 0 otherwise
  */
 int read_file(Board* pboard, char* path); 
 
 /**
  * \fn void create_empty_board(Board* pboard)
- * \brief Function which initialize an empty board
- * \param  pboard : A board pointer, can't be NULL
- * \return void
+ * \brief Initialize an empty Board
+ *
+ * \param pboard : Pointer to Board, SHOULD be NULL
  */
 void create_empty_board(Board* pboard); 
 
 /**
  * \fn Pcoord* get_valid_moves(Board* pboard)
- * \brief Function which gives a valid moves list 
- * \param  pboard : A board pointer, can't be NULL
- * \return a Coord list
+ * \brief Give a list of all valid moves.
+ *
+ * \param pboard Board pointer
+ * \return A Pcoord list
  */
 Pcoord* get_valid_moves(Board* pboard);
 
 /**
  * \fn int is_move_valid(Board* pboard,Pcoord pcoord);
- * \brief Function which verify that a move is valid
- * \param  pboard : A board pointer, can't be NULL
- * \param  pcoord : The coordinates of the selected move , can't be NULL
- * \return (1 : true)  if no errors occured else return ( 0 : false )
+ * \brief Check if a move is valid.
+ *
+ * \param pboard Board pointer
+ * \param pcoord Coordinates of the selected move
+ * \return 1 if no error occured, 0 otherwise
  */
 int is_move_valid(Board* pboard,Pcoord pcoord);
 
 /**
  * \fn Board* initialize_rand()
- * \brief Function which initialize a board randomly
- * \return A pointer to a board
+ * \brief Randomly initalize a Board.
+ ù
+ * \return Pointer to a Board
  */
 Board* initialize_rand();
 
 /**
  * \fn int play_move(Board* pboard, Pcoord pcoord)
- * \brief Add a point on the board.
+ * \brief Add a point on the Board.
  *
- * \param pboard Pointer to the board being played.
+ * \param pboard Pointer to the Board being played.
  * \param pcoord Pointer to selected coordinates.
  * \return 1 if point successfully added, 0 otherwise.
  */
@@ -104,8 +105,9 @@ int play_move(Board* pboard,Pcoord pcoord);
 
 /**
  * \fn int is_game_over(Board* pboard);
- * \brief check that no points can be added 
- * \param pboard Pointer to the board being played.
+ * \brief Check if game is over. 
+ *
+ * \param pboard Pointer to the Board being played.
  * \return 1 if game is over, 0 otherwise.
  */
 int is_game_over(Board* pboard);
