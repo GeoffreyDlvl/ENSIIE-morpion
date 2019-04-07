daeb2442a19eb6a45fd516fba4e0da8d4cd1f288
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2019-04-07 21:01:58 +0200

    Add resolved path support when file passed as argument

--- sources/main.c
+++ sources/main.c
@@ -1,17 +1,18 @@
 #include "../headers/board.h"
 #include "../headers/interface.h"
 #include "../headers/history.h"
-#include<stdio.h>                                                               
-#include<stdlib.h>                                                              
-#include<unistd.h>                                                              
-#include<sys/types.h>                                                           
-#include<sys/stat.h>                                                            
-#include<fcntl.h>                                                               
-#include<string.h>                                                              
-#include<errno.h>  
-#include <time.h>                                                             
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <linux/limits.h>
                                                                                 
-int main(int argc, char* argv[]){   
+int main(int argc, char* argv[]){
   srand(time(NULL));
   /*If user gives more than 2 arguments*/
   if (argc>3){
@@ -27,7 +28,8 @@ int main(int argc, char* argv[]){
     /*If first argument is neither option -r and -h
     OR
     first argument is option -r but is not followed by a parameter*/
-    if ((strcmp(argv[1],"-r")!=0 && strcmp(argv[1],"-h")!=0)||(strcmp(argv[1],"-r")==0 && argc==2)){
+    if ( (strcmp(argv[1],"-r") != 0 && strcmp(argv[1],"-h") != 0) ||
+            ((strcmp(argv[1],"-r")==0) && argc==2) ) {
       fprintf(stderr,"%s:FATAL: %s invalid args (none, -r FILE or -h expected) \n",argv[0],argv[1]);
       return EXIT_FAILURE;
     }
@@ -36,11 +38,19 @@ int main(int argc, char* argv[]){
       print_help();
       return EXIT_SUCCESS;
     }
+    /* A file has been passed as argument */
     else{
-      /* If file given as a parameter (following option -r) is invalid*/
-      if (0){// (!read_file(pboard,argv[2])){
-        return EXIT_FAILURE;
-      }
+        /* Get the file argument absolute path (i.e. resolved path) */
+        char resolved_path[PATH_MAX];
+        realpath(argv[2], resolved_path);
+
+        /* If file given as a parameter is invalid*/
+        if (!check_file(resolved_path)) {
+            return EXIT_FAILURE;
+        }
+        else {
+
+        }
     }
   }
   bool hint = false;
@@ -58,13 +68,8 @@ int main(int argc, char* argv[]){
     Move_print(get_points_history());
     print_board(&board, hint);
     playerAction = select_action();
-    if(playerAction == QUIT_GAME) {
-	/* Free all allocated pointers */
-  	free_history();
-  	remove_points(&board);
-  	free_board(&board);
-  	return EXIT_SUCCESS;
-    }
+    if(playerAction == QUIT_GAME)
+        break;
     execute_action(&board, playerAction);
     i++;
   }
