0525ccb13762e1d3731bdac2c8f7cba12c234d3c
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2019-04-12 16:59:51 +0200

    Add print board colors
    Hint blink in yellow.
    Lines in green.

--- sources/interface.c
+++ sources/interface.c
@@ -4,26 +4,13 @@
 #include <stdlib.h>
 #include <linux/limits.h>
 
-/* Color support */
+/* Colors and effects support */
 #define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
-
-#define BLINK   "\033[5m"
+#define RED     "\033[31m"              /* Red */
+#define BOLDRED     "\033[1m\033[31m"   /* Bold Red */
+#define LIGHTYELLOW "\033[93m"          /* Light Yellow */
+#define LIGHTGREEN   "\033[92m"         /* Light Green */
+#define BLINK   "\033[5m"               /* Blinking effect */
 
 static bool hint;
 
@@ -82,13 +69,13 @@ void print_board(Board* pboard)
       printf("[");
       if(pboard->points[i][j]) {
 	if (Move_search(get_lines_history(),j,i,indexLines)) {
-	  printf(MAGENTA "x" RESET);
+	  printf(LIGHTGREEN "x" RESET);
 	} else {
 	  printf("\u2022");
 	}
       }
       else if (hint && Move_search(possible_moves,j,i,indexLines)){
-	printf(BLINK "*" RESET);
+	printf(BLINK LIGHTYELLOW "*" RESET);
       }
       else {
 	printf(" ");
