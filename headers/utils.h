762dd5f3addc0e90bbabe1020b1d57298b4fcc11
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2019-04-15 12:54:30 +0200

    Finish documentation for utils.h

--- headers/utils.h
+++ headers/utils.h
@@ -1,11 +1,26 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/**
+ * \file utils.h
+ * \brief Utility file.
+ * \authors Geoffrey DELVAL, Rémi GUIJARRO ESPINOSA, Gabriel MORAN, Clément PERESSE
+ * \version lot_b
+ * \date 04.2019
+ *
+ * Handles various functions that had no place in project files, that is to say
+ * error handling, file checking, installing dependencies, etc.
+ *
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 
-
-typedef enum error
+/**
+ * \enum Error
+ * \brief All errors.
+ */
+typedef enum
 {
     WRONG_INPUT_ERR, /**< Wrong user input */
     FILE_PTR_ERR, /**< File could not be opened */
@@ -14,26 +29,58 @@ typedef enum error
     POINT_ALREADY_EXIST_ERR, /**< The point already exists */
     INVALID_COORDINATES_ERR, /**< Coordinates are invalid */
     ALIGNMENT_ERR, /**< Alignment is incorrect */
-    /* ADD MORE ENUMERATIONS IF REQUIRED */
 } Error;
 
+/**
+ * \fn void install_dependencies(void);
+ * \brief Install required dependencies on user machine.
+ */
 void install_dependencies(void);
 
+/**
+ * \fn size_t get_file_board_width(FILE *fp);
+ * \brief Get the width of the Board represented by a file.
+ *
+ * \param fp FILE pointer.
+ * \return The width of the Board.
+ */
 size_t get_file_board_width(FILE *fp);
 
+/**
+ * \fn size_t get_file_board_height(FILE *fp);
+ * \brief Get the height of the Board represented by a file.
+ *
+ * \param fp FILE pointer.
+ * \return The height of the Board.
+ */
 size_t get_file_board_height(FILE *fp);
 
 /**
- * \fn bool check_file(char* path)
+ * \fn bool check_file(FILE *fp, Error* err)
  * \brief Check the integrity and structure of a file.
  *
- * \param path : the path to the file to verify
- * \return true if file structure is correct, false otherwise
+ * \param fp FILE pointer
+ * \param err Pointer to Error that will hold potential Error (if file is incorrect).
+ * \return True if file structure is correct, false otherwise
  */
 bool check_file(FILE *fp, Error* err);
 
+/**
+ * \fn void empty_input_buffer(void);
+ * \brief Empty the input buffer.
+ */
 void empty_input_buffer(void);
 
+/**
+ * \fn int get_random_number(int min, int max);
+ * \brief Generate a random integer between min and max.
+ *
+ * Random number generator must be seeded.
+ *
+ * \param min Min value.
+ * \param max Max value.
+ * \return Integer between min and max.
+ */
 int get_random_number(int min, int max);
 
 #endif
