035d74245d3b7ca3666b338af398a567599be489
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2019-04-17 20:19:21 +0200

    Improve unit tests

--- tests/test.c
+++ tests/test.c
@@ -13,7 +13,7 @@ static Error* error;
  * Returns the zero on success, non-zero otherwise.
  */
 int init_suite(void){
-    error = (Error*) malloc(sizeof(Error));
+    error = malloc(sizeof(Error));
     system("date");
     return 0;
 }
@@ -31,38 +31,83 @@ int clean_suite(void)
 /************* TESTS *************/
 void test_read_file_char_is_invalid(void)
 {
+    *error = NO_ERR;
     char resolved_path[PATH_MAX];
     realpath("test-files/board-invalid-char", resolved_path);
     FILE *fp = fopen(resolved_path, "r");
+    /* Check return value */
     CU_ASSERT_FALSE(check_file(fp,error))
+    /* Deeper test: check error value */
+    CU_ASSERT(*error == FILE_UNKNOWN_CHAR_ERR);
     fclose(fp);
     free(fp);
 }
 
 void test_read_file_dimensions_are_invalid(void)
 {
+    *error = NO_ERR;
     char resolved_path[PATH_MAX];
     realpath("test-files/board-invalid-dimensions", resolved_path);
     FILE *fp = fopen(resolved_path, "r");
+    /* Check return value */
     CU_ASSERT_FALSE(check_file(fp,error));
+    /* Deeper test: check error value */
+    CU_ASSERT(*error == FILE_DIMENSION_ERR);
     fclose(fp);
     free(fp);
 }
 
+void test_read_file_path_is_invalid(void)
+{
+    *error = NO_ERR;
+    char resolved_path[PATH_MAX];
+    realpath("path/is/invalid", resolved_path);
+    FILE *fp = fopen(resolved_path, "r");
+    /* Check return value */
+    CU_ASSERT_FALSE(check_file(fp,error));
+    /* Deeper test: check error value */
+    CU_ASSERT(*error == FILE_PTR_ERR);
+    if(fp) {
+        fclose(fp);
+        free(fp);
+    }
+}
+
 void test_read_file_board_is_valid(void)
 {
+    *error = NO_ERR;
     char resolved_path[PATH_MAX];
     realpath("test-files/board-valid", resolved_path);
     FILE *fp = fopen(resolved_path, "r");
+    /* Check return value */
     CU_ASSERT(check_file(fp,error));
+    /* Deeper test: check error value */
+    CU_ASSERT(*error == NO_ERR);
     fclose(fp);
     free(fp);
 }
 
 void test_adding_already_existing_points(void)
 {
-    Board* pboard = (Board*) malloc(sizeof(Board));
-    *pboard = create_empty_board(10,10);
+    Board board;
+    *error = NO_ERR;
+    char resolved_path[PATH_MAX];
+    realpath("test-files/board-valid", resolved_path);
+    FILE *fp = fopen(resolved_path, "r");
+    /* Check that initialization went well */
+    CU_ASSERT(initialize_file(&board, resolved_path, err))
+    /* Check return value of a played move */
+        /* Create the coord of the played move */
+        Coord coord;
+        coord.x = 3;
+        coord.y = 0;
+    /* I NEED TO FINISH */
+    CU_ASSERT_FALSE(play_move(&board, ));
+    /* Deeper test: check error value */
+    CU_ASSERT(*error == NO_ERR);
+    fclose(fp);
+    free(fp);
+    board = create_empty_board(10,10);
     int* point = (int*) malloc(sizeof(int));
     *point =1;
     pboard->points[0][0] = point;
@@ -79,7 +124,7 @@ void test_is_move_valid(void){
     Board* pboard;
     char resolved_path[PATH_MAX];
     realpath("test-files/board-valid-move", resolved_path);
-    initialize_file(pboard,resolved_path);
+    initialize_file(pboard,resolved_path,error);
 }
 
 void test_creating_empty_board(void){
@@ -109,6 +154,8 @@ int main(void)
     if((NULL == CU_add_test(pSuite, "Read_board_file_invalid_char", test_read_file_char_is_invalid))
         ||
         (NULL == CU_add_test(pSuite, "Read_board_file_dimensions_invalid", test_read_file_dimensions_are_invalid))
+        ||
+        (NULL == CU_add_test(pSuite, "Read_board_file_path_invalid", test_read_file_path_is_invalid))
         ||
         (NULL == CU_add_test(pSuite, "Read_board_valid", test_read_file_board_is_valid))
         ||
